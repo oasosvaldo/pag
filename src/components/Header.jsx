6fa125c3da61e7b95761b74ccd73ef9b16073bf2
OsvaldoSilveira <oasosvaldo@gmail.com>
2023-11-14 01:47:11 -0300

    Logo
    Coloquei uma logo e fiz algumas alteracoes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
+import Logo from '../imgs/LogoKart.png'
 
 const Navbar = () => {
   return (
-    <div className='cssmenu'>        
+    <div className='cssmenu'>      
+      <div className='Logo'><Link to="/"><img src={Logo} alt="Logo" /></Link></div>        
       <ul>
         <li className='last'><Link to="/">Home</Link></li>
         <li><Link to="/empresa">Empresa</Link></li>
