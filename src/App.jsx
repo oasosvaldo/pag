74d068fe0b15650132484e4ab24de6bee03c90b3
OsvaldoSilveira <oasosvaldo@gmail.com>
2024-01-07 15:58:18 -0300

    navegando em paginas

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
-      <Header></Header>
+      <Header />
       <main>
         <Switch>
           <Route exact path="/"><Home /></Route>
